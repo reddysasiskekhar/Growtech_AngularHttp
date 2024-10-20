c19da68d213fa48d9424217486624fdbab741588
osasisekharreddy <sasisekhar999@gmail.com>
2024-10-20 18:51:34 +0530

    http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Growtech_AngularHttp';
+export class AppComponent implements OnInit {
+  // title = 'Growtech_AngularHttp';
+
+  constructor(public httpClient: HttpClient) {
+
+  }
+
+  bookmyShowUrl ="https://in.bookmyShow.com/api/explore/v1/discover/regions"
+  jsonplaceholderURL = "https://jsonplaceholder.typicode.com/posts";
+
+  output:any[] = [];
+  ngOnInit(): void {
+    this.httpClient.get(this.jsonplaceholderURL).subscribe((data: any) => {
+      console.log("sasi",data);
+      this.output = data;// this is arry of obect
+    });
+
+    this.httpClient.get(this.bookmyShowUrl).subscribe((data: any) => {
+      console.log("sekhar-BookMyshow",data);
+      // this.output = data;// this is arry of obect
+    });
+  }
 }
